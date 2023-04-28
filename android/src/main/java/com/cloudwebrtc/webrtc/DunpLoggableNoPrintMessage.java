ed6a1b78c3552a4c4bf6351b8c97488359a05abb
dunp <dunp@omt.vn>
2023-04-28 11:07:43 +0700

    DunpLoggableNoPrintMessage

--- android/src/main/java/com/cloudwebrtc/webrtc/DunpLoggableNoPrintMessage.java
+++ android/src/main/java/com/cloudwebrtc/webrtc/DunpLoggableNoPrintMessage.java
@@ -8,13 +8,6 @@ class DunpLoggableNoPrintMessage implements org.webrtc.Loggable {
 
     @Override
     public void onLogMessage(String s, Logging.Severity severity, String s1) {
-        //BufferQueueProducer //SurfaceTexture
-//        if (s.indexOf("BufferQueueProducer") <= 0 || s.indexOf("SurfaceTexture") <= 0 || s.indexOf("queueBuffer")<=0) {
-//            Log.i("dunp", s);
-//            Log.i("dunp", s1);
-//        }
-//        if (s1.indexOf("BufferQueueProducer") <= 0 || s1.indexOf("SurfaceTexture") <= 0|| s.indexOf("queueBuffer")<=0) {
-//            Log.i("dunp", s1);
-//        }
+
     }
 }
